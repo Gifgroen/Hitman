27dc7194ba3b16b91a2e6ebcaecfca1b39b0196b
Karsten Westra <karstenwestra+github@gmail.com>
2022-12-13 23:28:37 +0100

    Implemented basic window test with SDL2.

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,9 @@
-@echo off
+CC=gcc
+
+SDL2_INCLUDE_FLAGS="-Iw:\hitman\lib\SDL2-2.26.1\x86_64-w64-mingw32\include"
+SDL2_LINKER_FLAGS="`W:/hitman/lib/SDL2-2.26.1/x86_64-w64-mingw32/bin/sdl2-config --cflags --libs`"
 
 mkdir -p w:/hitman/build
 pushd w:/hitman/build
-cl -Zi w:/hitman/src/win32_awe.cpp
+$CC -g3 w:/hitman/src/win32_awe.cpp -o w:/hitman/build/win32_awe.exe $SDL2_INCLUDE_FLAGS $SDL2_LINKER_FLAGS
 popd
--- src/win32_awe.cpp
+++ src/win32_awe.cpp
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main() {
-    printf("Hello AWE!\n");
-    return(0);
+#define global static
+#define internal static
+#define local_persist static
+
+global bool Running = true;
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 1024
+
+global SDL_Window *gWindow = NULL;
+
+int main(int argc, char *argv[]) 
+{
+    printf("Running AWE!\n");
+
+    if(SDL_Init(SDL_INIT_VIDEO)) 
+    {
+        printf("Cannot init video\n");
+        return 1;
+    }
+
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Event e;
+
+    while(Running) 
+    {
+        while(SDL_PollEvent(&e) != 0)
+        {
+            if(e.type == SDL_QUIT)
+            {
+                Running = false;
+            }
+        }
+    }
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+ 
+    return 0;
 }
