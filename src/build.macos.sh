8fb1fc1788d409791b4df017a071b3a743273a8f
Karsten Westra <karstenwestra@gmail.com>
2022-12-18 15:41:19 +0100

    In build scripts; renamed build artefacts to match type of build created.

--- src/build.macos.sh
+++ src/build.macos.sh
@@ -3,5 +3,5 @@ BASE_DIR="/Users/me/Projects/Games/Hitman/"
 
 mkdir -p $BASE_DIR/build
 pushd $BASE_DIR/build
-$CC -g3 $BASE_DIR/src/win32_awe.cpp -o $BASE_DIR/build/win32_awe.out `sdl2-config --cflags --libs`
+$CC -g3 $BASE_DIR/src/platform_awe.cpp -o $BASE_DIR/build/macos_awe.out `sdl2-config --cflags --libs`
 popd
--- src/build.win32.sh
+++ src/build.win32.sh
@@ -5,5 +5,5 @@ SDL2_LINKER_FLAGS="`W:/hitman/lib/SDL2-2.26.1/x86_64-w64-mingw32/bin/sdl2-config
 
 mkdir -p w:/hitman/build
 pushd w:/hitman/build
-$CC -g3 w:/hitman/src/win32_awe.cpp -o w:/hitman/build/win32_awe.exe $SDL2_INCLUDE_FLAGS $SDL2_LINKER_FLAGS
+$CC -g3 w:/hitman/src/platform_awe.cpp -o w:/hitman/build/win32_awe.exe $SDL2_INCLUDE_FLAGS $SDL2_LINKER_FLAGS
 popd
