0a83a0189ca7297508d215a2e563aacda6830074
Karsten Westra <karsten@gpal.nl>
2022-12-20 15:44:33 +0100

    Simplified macos build script.

--- src/build.macos.sh
+++ src/build.macos.sh
@@ -1,19 +1,15 @@
 CC=gcc
-
-INITIAL_WORKING_DIRECTORY=$(pwd)
-BASE_DIR="${HOME}/Projects/Games/Hitman/"
-
 COMMON_COMPILER_FLAGS="-g3 -Wall -std=c++11"
-PLATFORM_LINKER_FLAGS="-L$BASE_DIR/build -lhitman"
 SDL2_FLAGS="`sdl2-config --cflags --libs`"
 
+BASE_DIR="${HOME}/Projects/Games/Hitman/"
 mkdir -p $BASE_DIR/build
 pushd $BASE_DIR/build
 
 # Create Game service
-$CC $COMMON_COMPILER_FLAGS -shared -o $BASE_DIR/build/libhitman.so -fPIC $BASE_DIR/src/hitman.cpp 
+$CC $COMMON_COMPILER_FLAGS -shared -o ./libhitman.so -fPIC $BASE_DIR/src/hitman.cpp 
 
 # Create platform layer that uses platform agnostic Game
-$CC $COMMON_COMPILER_FLAGS -o $BASE_DIR/build/hitman_macos -fPIC $BASE_DIR/src/platform_macos.cpp $SDL2_FLAGS
+$CC $COMMON_COMPILER_FLAGS -o hitman_macos -fPIC $BASE_DIR/src/platform_macos.cpp $SDL2_FLAGS
 
 popd
