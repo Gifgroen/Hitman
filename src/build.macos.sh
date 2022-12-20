27f0cc104180fde1230e5955f951681cd23be52f
Karsten Westra <karstenwestra+github@gmail.com>
2022-12-20 20:25:47 +0100

    Fixed windows build and moved macos platform code to general platform, since it also works on windows 11.

--- src/build.macos.sh
+++ src/build.macos.sh
@@ -1,8 +1,10 @@
 CC=gcc
-COMMON_COMPILER_FLAGS="-g3 -Wall -std=c++11"
-SDL2_FLAGS="`sdl2-config --cflags --libs`"
 
 BASE_DIR="${HOME}/Projects/Games/Hitman/"
+
+COMMON_COMPILER_FLAGS="-g3 -Wall -std=c++11"
+SDL2_LINKER_FLAGS="`sdl2-config --cflags --libs`"
+
 mkdir -p $BASE_DIR/build
 pushd $BASE_DIR/build
 
@@ -10,6 +12,6 @@ pushd $BASE_DIR/build
 $CC $COMMON_COMPILER_FLAGS -shared -o ./libhitman.so -fPIC $BASE_DIR/src/hitman.cpp 
 
 # Create platform layer that uses platform agnostic Game
-$CC $COMMON_COMPILER_FLAGS -o hitman_macos -fPIC $BASE_DIR/src/platform_macos.cpp $SDL2_FLAGS
+$CC $COMMON_COMPILER_FLAGS -o hitman_macos -fPIC $BASE_DIR/src/platform.cpp $SDL2_LINKER_FLAGS
 
 popd
--- src/build.win32.sh
+++ src/build.win32.sh
@@ -1,9 +1,23 @@
 CC=gcc
+COMMON_COMPILER_FLAGS="-g3 -Wall -std=c++11"
 
-SDL2_INCLUDE_FLAGS="-Iw:\hitman\lib\SDL2-2.26.1\x86_64-w64-mingw32\include"
-SDL2_LINKER_FLAGS="`W:/hitman/lib/SDL2-2.26.1/x86_64-w64-mingw32/bin/sdl2-config --cflags --libs`"
+BASE_DIR="${HOME}/Projects/Games/Hitman/"
 
-mkdir -p w:/hitman/build
-pushd w:/hitman/build
-$CC -g3 w:/hitman/src/platform_awe.cpp -o w:/hitman/build/win32_awe.exe $SDL2_INCLUDE_FLAGS $SDL2_LINKER_FLAGS
+SDL2_INCLUDE_FLAGS="-I$BASE_DIR\lib\SDL2-2.26.1\x86_64-w64-mingw32\include"
+SDL2_LINKER_FLAGS="`$BASE_DIR/lib/SDL2-2.26.1/x86_64-w64-mingw32/bin/sdl2-config --cflags --libs`"
+
+COMMON_LINKER_FLAGS="-ldl"
+
+mkdir -p $BASE_DIR/build
+pushd $BASE_DIR/build
+
+# Create Game service
+$CC $COMMON_COMPILER_FLAGS -shared -o ./libhitman.so -fPIC $BASE_DIR/src/hitman.cpp 
+
+# Create platform layer that uses platform agnostic Game
+$CC $COMMON_COMPILER_FLAGS -o hitman_win32.exe -fPIC $BASE_DIR/src/platform.cpp $SDL2_INCLUDE_FLAGS $SDL2_LINKER_FLAGS $COMMON_LINKER_FLAGS
+
+# TODO: check iof we can add a DLL search path
+cp $BASE_DIR/lib/SDL2-2.26.1/x86_64-w64-mingw32/bin/SDL2.dll .
+
 popd
